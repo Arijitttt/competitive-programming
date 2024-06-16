b1df50b59b6f2953f7d853669cd68be2d1d7ab7f
Arijit Bhattacharya <113541100+Arijitttt@users.noreply.github.com>
2024-06-16 17:16:09 +0530

    python

---
+++ PYTHON/03basicProgram/APSeries.py
@@ -0,0 +1,6 @@
+ft = float(input("enter 1st term: "))
+cd = float(input("enter common difference: "))
+nt = int(input("enter no of terms: "))
+for i in range(nt):
+    print(ft, end=" ")
+    ft+=cd
--- PYTHON/03basicProgram/fibonacci.py
+++ PYTHON/03basicProgram/fibonacci.py
@@ -16,4 +16,4 @@ def fibonacci(num):
     while(len(fib_series)<num):
         fib_series.append(fib_series[-1]+fib_series[-2])
     return fib_series
-print(fibonacci(1))
+print(fibonacci(20))

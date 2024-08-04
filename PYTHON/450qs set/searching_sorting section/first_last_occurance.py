b0f55d8276633e5fc67245f01a50e685b443c841
Arijit Bhattacharya <113541100+Arijitttt@users.noreply.github.com>
2024-08-04 18:37:41 +0530

    optimized way

--- PYTHON/450qs set/searching_sorting section/first_last_occurance.py
+++ PYTHON/450qs set/searching_sorting section/first_last_occurance.py
@@ -37,19 +37,50 @@
 #             # while arr[i] == arr[j]:
 #             #     print('')
 #     return -1
+
+
+
+# def first_last_occurance(arr,n,x):
+#     first  = -1
+#     last = -1
+#     for i in range(n):
+#         if arr[i]==x:
+#             if first == -1:
+#                 first = i
+#             last = i
+#     if first != -1 and last != -1:
+#         return first,last
+#     else:
+#         return -1
+
+#optimized approach
 def first_last_occurance(arr,n,x):
-    first  = -1
-    last = -1
-    for i in range(n):
-        if arr[i]==x:
-            if first == -1:
-                first = i
-            last = i
-    if first != -1 and last != -1:
-        return first,last
-    else:
+    def find_first(arr,low,high,x):
+        if high >= low:
+            mid = (low+high)//2
+            if (mid == 0 or x>arr[mid-1]) and arr[mid]==x:
+                return mid
+            elif x>arr[mid]:
+                return find_first(arr,mid+1,high,x)
+            else:
+                return find_first(arr,low,mid-1,x)
         return -1
 
+    def find_last(arr,low,high,x):
+        if low<=high:
+            mid = (low+high)//2
+            if(mid == n-1 or x<arr[mid+1]) and x==arr[mid]:
+                return mid
+            elif x<arr[mid]:
+                return find_last(arr,low,mid-1,x)
+            else:
+                return find_last(arr,mid+1,high,x)
+        return -1
+    first = find_first(arr,0,n-1,x)
+    if first == -1:
+        return -1
+    last = find_last(arr,0,n-1,x)
+    return first,last
 arr = [1, 3, 5, 5, 5, 5, 67, 123, 125]
 print(first_last_occurance(arr, len(arr), 5))
 

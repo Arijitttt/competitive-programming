79a2f6072c76c2976752738459225810fc57dbcf
Arijitttt <abhattacharya030@gmail.com>
2024-11-09 03:08:16 +0530

    tree practice done

--- PYTHON/15tree/overallPractice.py
+++ PYTHON/15tree/overallPractice.py
@@ -1,81 +1,72 @@
 from collections import deque
 class Queue(object):
     def __init__(self):
-        self.arr = deque()
+        self.queue = deque()
     def enqueue(self,item):
-        self.arr.append(item)
+        self.queue.append(item)
+    def is_empty(self):
+        return len(self.queue) == 0
     def dequeue(self):
         if not self.is_empty():
-            return self.arr.popleft()
-    def is_empty(self):
-        return len(self.arr) == 0
+            return self.queue.popleft()
     def peek(self):
         if not self.is_empty():
-            return self.arr[0]
-    def length(self):
-        return len(self.arr)
+            return self.queue[0]
+    def size(self):
+        return len(self.queue)
 class Node(object):
     def __init__(self,value):
         self.value = value
-        self .left = None
+        self.left = None
         self.right = None
 
 class BinaryTree(object):
     def __init__(self,root):
         self.root = Node(root)
-
     def preOrder(self,start,traversal):
-        '''root left right'''
-        if start:
-            traversal += (str(start.value) + '-')
+        # root left right
+        if start: 
+            traversal += (str(start.value)+ '-')
             traversal = self.preOrder(start.left,traversal)
             traversal = self.preOrder(start.right,traversal)
         return traversal
-    
-    def inorder(self,start,traversal):
-        # left root right
+    def inOrder(self,start,traversal):
         if start:
-            traversal = self.inorder(start.left,traversal)
-            traversal += (str(start.value) + '-')
-            traversal = self.inorder(start.right,traversal)
-
+            traversal = self.inOrder(start.left,traversal)
+            traversal += (str(start.value) + "-")
+            traversal = self.inOrder(start.right,traversal)
         return traversal
-    
     def postOrder(self,start,traversal):
-        # left right root
         if start:
             traversal = self.postOrder(start.left,traversal)
             traversal = self.postOrder(start.right,traversal)
             traversal += (str(start.value) + '-')
         return traversal
     
-    def levelorder(self,start):
-        if start is None: 
+    def levelOrder(self,start):
+        if start is None:
             return
         queue = Queue()
-        queue.enqueue(start)
         traversal = ''
-        while queue.length() > 0:
+        queue.enqueue(start)
+        while queue.size() > 0:
             traversal += str(queue.peek().value) + '-'
-            node = queue.dequeue()
+            node  = queue.dequeue()
             if node.left:
                 queue.enqueue(node.left)
             if node.right:
                 queue.enqueue(node.right)
         return traversal
     
-    def printTree(self,traversal_type):
-        if traversal_type == 'preorder':
+    def print_tree(self,traversalType):
+        if traversalType == 'preorder':
             return self.preOrder(tree.root,'')
-        elif traversal_type == 'inorder':
-            return self.inorder(tree.root,'')
-        elif traversal_type == 'postorder':
+        elif traversalType == 'inorder':
+            return self.inOrder(tree.root,'')
+        elif traversalType == 'postorder':
             return self.postOrder(tree.root,'')
-        elif traversal_type == 'levelorder':
-            return self.levelorder(tree.root)
-        else:
-            print( 'Traversal type ' + traversal_type + ' is not supported')
-            return False
+        elif traversalType == 'levelorder':
+            return self.levelOrder(tree.root)
 
 
 tree = BinaryTree(1)
@@ -86,5 +77,5 @@ tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 
-print(tree.printTree('preorder'))
-print(tree.printTree('levelorder'))
+print(tree.print_tree('preorder'))
+print(tree.print_tree('levelorder'))
